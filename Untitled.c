8a1d5c62cbf9ae9510a4057788faa68d4af5e1e9
GAURAV  KUMAR <endofthelinecoeb3@gmail.com>
2021-02-19 15:52:58 +0530

    Untitled-1.c

---
+++ Untitled.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+     int n,r,s1=0,r2=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        r2=(r2*10)+r;
+        n=n/10;
+    }
+    while(r2!=0)
+    {
+      r=r2%10;
+        if(r==0)
+            r=5;
+        s1=(s1*10)+r;
+        r2=r2/10;
+    }
+    printf("%d",s1);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
